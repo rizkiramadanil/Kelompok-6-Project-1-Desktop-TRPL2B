ab645d66531e2c191cae89d336c06d4b19c8c721
rizkiramadanil <rizkiramadanil000@gmail.com>
2021-05-28 06:43:56 +0700

    update pertama

---
+++ src/sisteminformasipuskesmas/connection.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisteminformasipuskesmas;
+
+/**
+ *
+ * @author Asus
+ */
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class connection {
+    private static String servername = "localhost";
+    private static String username = "root";
+    private static String dbname = "puskesmas";
+    private static Integer portnumber = 3306;
+    private static String password = "";
+    
+    public static Connection getConnection()
+    {
+        Connection sip = null;
+        
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setPassword(password);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        
+        try {
+            sip = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger("Get Connection -> " + connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        return sip;
+    }
+}
